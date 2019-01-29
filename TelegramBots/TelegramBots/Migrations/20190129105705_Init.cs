ab241e8279404b0058c591e7375567edca28e8b1
Aleksey Boikovskyi <aleksey.boikovskyi@trustsourcing.com>
2019-01-29 13:17:18 +0200

    migrations

---
+++ TelegramBots/TelegramBots/Migrations/20190129105705_Init.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TelegramBots.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(nullable: false),
+                    ContactFirstName = table.Column<string>(nullable: true),
+                    ContactLastName = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    ZoneId = table.Column<int>(nullable: true),
+                    NumberOfPeople = table.Column<int>(nullable: true),
+                    GameConsole = table.Column<int>(nullable: true),
+                    Game = table.Column<string>(nullable: true),
+                    RequestDate = table.Column<DateTime>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    UserPhone = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRequests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRequests");
+        }
+    }
+}
