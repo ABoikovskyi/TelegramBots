46d6b01bd9447c83e08cb7c40b61b90b857a9d2f
Aleksey Boikovskyi <aleksey.boikovskyi@trustsourcing.com>
2019-07-16 13:29:07 +0300

    link

--- TelegramBots/BusinessLayer/Helpers/Links.cs
+++ TelegramBots/BusinessLayer/Helpers/Links.cs
@@ -2,8 +2,8 @@
 {
 	public class Links
 	{
-		//public const string AppLink = "https://idrink.com.ua";
-		public const string AppLink = "https://59612235.ngrok.io";
+		public const string AppLink = "https://idrink.com.ua";
+		//public const string AppLink = "https://59612235.ngrok.io";
 		public const string TelegramKey = "740230403:AAEY_iB-EA_v63uiTXj7ohEq2rgqbqxeojc";
 	}
 }
