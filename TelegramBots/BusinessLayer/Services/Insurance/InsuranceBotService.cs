778f4529820571f29d8abc6be4d610ed3a74d095
Aleksey Boikovskyi <aleksey.boikovskyi@trustsourcing.com>
2020-07-23 12:45:55 +0300

    unused

--- TelegramBots/BusinessLayer/Services/Insurance/InsuranceBotService.cs
+++ TelegramBots/BusinessLayer/Services/Insurance/InsuranceBotService.cs
@@ -14,7 +14,6 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InputFiles;
-using Telegram.Bot.Types.ReplyMarkups;
 
 namespace BusinessLayer.Services.Insurance
 {
